b27af79c8fed0f03c157f00351e7bfb6aaf77149
Jeff Gough <j@jeffreygough.com>
2024-01-30 21:11:56 +0000

    Handler for "set" command.

--- sound_config.py
+++ sound_config.py
@@ -4,13 +4,25 @@ import hardware_drivers.soundcard as soundcard
 sc = soundcard.Soundcard()
 
 def handle_power_command(args):
+    """ Turn the soundcard on / off """
     if args.parameter == 'on':
         sc.enable()
     elif args.parameter == 'off':
         sc.disable()
 
 def handle_set_command(args):
-    pass
+    """ Set gain and phantom power """
+    sc.set_gain(args.gain)
+
+    match args.phantom:
+        case 'none':
+            sc.set_phantom(sc.NONE)
+        case 'PIP':
+            sc.set_phantom(sc.PIP)
+        case '3V3':
+            sc.set_phantom(sc.P3V3)
+        case 'P48':
+            sc.set_phantom(sc.P48)
 
 def main():
     parser = argparse.ArgumentParser(description='Test sound commands.')
@@ -23,8 +35,8 @@ def main():
 
     # Set command
     set_parser = subparsers.add_parser('set', help='Control gain and phantom power')
-    set_parser.add_argument('--gain', '-g', required=True, type=int, choices=range(0, 61), metavar='[0-60]', help='Set gain: 0-60dB in 3dB steps')
-    set_parser.add_argument('--phantom', '-p', required=True, choices=['NONE','P48', 'PIP', '3V3'], help='set power mode')
+    set_parser.add_argument('--gain', '-g', required=True, type=int, choices=range(0, 32), metavar='[0-21]', help='Set gain: 3dB steps, 0-60dB')
+    set_parser.add_argument('--phantom', '-p', required=True, choices=['none','P48', 'PIP', '3V3'], help='set power mode')
     set_parser.set_defaults(func=handle_set_command)
 
     args = parser.parse_args()
@@ -34,6 +46,7 @@ def main():
         args.func(args)
     else:
         parser.print_help()
+
 
 if __name__ == "__main__":
     main()
