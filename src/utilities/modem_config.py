e56184b1b45f946817b0464c47216295ee03a792
Jeff Gough <j@jeffreygough.com>
2024-03-20 21:42:27 +0000

    Rewrite standalone modem config script to use the modem package

--- src/utilities/modem_config.py
+++ src/utilities/modem_config.py
@@ -1,16 +1,43 @@
-import RPi.GPIO as GPIO
-import time 
+""" 
+Standalone utility to control the modem's power state and check status
+This allows the user to turn on the modem without running the recording application.
+It's mainly intended for use during debugging.  
+"""
 
-en_pin=7
-power_on_n_pin = 5
+import logging
+from ..hardware_drivers.modem import Modem
+# en_pin=7
+# power_on_n_pin = 5
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(en_pin, GPIO.OUT)
-GPIO.setup(power_on_n_pin, GPIO.OUT)
+# GPIO.setup(en_pin, GPIO.OUT)
+# GPIO.setup(power_on_n_pin, GPIO.OUT)
 
-GPIO.output(en_pin,1)
-time.sleep(1)
-GPIO.output(power_on_n_pin,1)
-time.sleep(1)
-GPIO.output(power_on_n_pin,0)
+# GPIO.output(en_pin,1)
+# time.sleep(1)
+# GPIO.output(power_on_n_pin,1)
+# time.sleep(1)
+# GPIO.output(power_on_n_pin,0)
+
+def main():
+    """ 
+    Standalone utility to control the modem's power state and check status
+    This allows the user to turn on the modem without running the recording application.
+    It's mainly intended for use during debugging.  
+    """
+    # Create a StreamHandler for stdout
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stdout_handler.setFormatter(formatter)
+
+    # Configure the root logger to use the stdout handler
+    logging.basicConfig(level=logging.INFO, handlers=[stdout_handler])
+    logger = logging.getLogger(__name__)
+    logger.info("Starting modem configuration utility.")
+    
+
+    modem = Modem()
+
+if __name__ == "__main__":
+    main()
