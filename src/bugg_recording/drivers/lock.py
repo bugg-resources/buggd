247e30e2c7b1b2ed2776efd5fffe58a0ada40b2f
Jeff Gough <j@jeffreygough.com>
2024-03-22 14:15:02 +0000

    Add doc string

--- src/bugg_recording/drivers/lock.py
+++ src/bugg_recording/drivers/lock.py
@@ -5,16 +5,22 @@ from filelock import FileLock, Timeout
 logging.getLogger().setLevel(logging.DEBUG)
 
 class Lock:
-    """ This class creates a lock file to prevent multiple instances of an applicatoin from accessing a resource. """
-    def __init__(self, lock_path):
-        self.lock_path = lock_path
+    """
+    For a given lock file, this class can only be instantiated once.
+    If another instance of the class is created with the same lock file, it will raise an exception. 
+    This is useful for ensuring that only one instance of a driver is running at a time, both 
+    within the same application and across multiple applications.
+    """
+
+    def __init__(self, lock_file):
+        self.lock_path = lock_file
         self.lock = FileLock(self.lock_path)
         try:
             # Attempt to acquire the lock
             self.lock.acquire(timeout=2)
             logging.debug("Lock acquired on %s", self.lock_path)
         except Timeout as e:
-            raise RuntimeError(f"Could not acquire lock on {lock_path} - Another instance is already running.") from e
+            raise RuntimeError(f"Could not acquire lock on {lock_file} - Another instance is already running.") from e
 
     def release_lock(self):
         """ Release the lock file """
