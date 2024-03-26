617e054234526656166fb96746ebf9b80ba40701
Jeff Gough <j@jeffreygough.com>
2024-03-26 23:45:34 +0000

    Rename fix

--- src/bugg_recording/sensors/__init__.py
+++ src/bugg_recording/sensors/__init__.py
@@ -1,4 +1,4 @@
 # Import individual sensor class files into the sensor module namespace
 # This does need to be edited as classes are added
 from .sensorbase import SensorBase
-from .I2SMic import I2SMic
+from .i2smic import I2SMic
