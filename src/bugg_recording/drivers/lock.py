df92b354d4c2c61284abaf1b44a1b29446784a44
Jeff Gough <j@jeffreygough.com>
2024-03-26 20:41:34 +0000

    Set debug level to Warning

--- src/bugg_recording/drivers/lock.py
+++ src/bugg_recording/drivers/lock.py
@@ -4,7 +4,7 @@ import logging
 from filelock import FileLock, Timeout
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 
 class Lock:
     """
