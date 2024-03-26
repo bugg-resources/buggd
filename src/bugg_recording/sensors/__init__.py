c68b26976f2fa4769f0edec8bb614c56246e6885
Jeff Gough <j@jeffreygough.com>
2024-03-26 23:35:35 +0000

    Move imports back into init

---
+++ src/bugg_recording/sensors/__init__.py
@@ -0,0 +1,4 @@
+# Import individual sensor class files into the sensor module namespace
+# This does need to be edited as classes are added
+from .sensorbase import SensorBase
+from .I2SMic import I2SMic
--- src/bugg_recording/sensors/options.py
+++ src/bugg_recording/sensors/options.py
@@ -1,9 +1,3 @@
-# Import individual sensor class files into the sensor module namespace
-# This does need to be edited as classes are added
-from .sensorbase import SensorBase
-from .I2SMic import I2SMic
-
-
 def set_option(var, config, opts):
     """
     Method to compare the provided and default config for a class variable
