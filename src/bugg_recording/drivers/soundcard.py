8270c5a02943752aaac82d76a42b779c75ecfde1
Jeff Gough <j@jeffreygough.com>
2024-03-26 20:10:54 +0000

    Read and write the soundcard gain and phantom settings to a temporary file. This allows us to independently set them in a sane way, because both settings must be supplied in the same SPI transaction.

--- src/bugg_recording/drivers/soundcard.py
+++ src/bugg_recording/drivers/soundcard.py
@@ -2,8 +2,18 @@
 
 import RPi.GPIO as GPIO
 import spidev
+import logging
+from .lock import Lock
+import os
+import json
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 EXT_MIC_EN = 12
+
+LOCK_FILE = "/tmp/soundcard.lock"
+STATE_FILE = "/tmp/soundcard_state.json"
 
 class Soundcard:
     """ Provides controls for the soundcard """
@@ -14,7 +24,15 @@ class Soundcard:
     P3V3 = "P3V3"   # 3.3V on M12 pin 4
     P48 = "P48"     # 48V
 
-    def __init__(self):
+    def __init__(self, lock_file_path=LOCK_FILE):
+        """ Attempt to acquire the lock and initialise the GPIO """
+        try:
+            self.lock = Lock(lock_file_path)
+        except RuntimeError as e:
+            logger.critical(e)
+            self.result = False
+            raise
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False) # Squash warning if the pin is already in use
         GPIO.setup(EXT_MIC_EN, GPIO.OUT)
@@ -27,9 +45,41 @@ class Soundcard:
         self.zc_gpo = 1     # Enable zero-crossing phantom switching by default
         self.phantom_mode = 0
 
+        self.state = {'gain':self.gain, 'phantom':self.phantom_mode}
+
+    def __del__(self):
+        """ Release the lock file when the object is deleted"""
+        self.lock.release_lock()
+
+    def store_state(self):
+        """ Write the hardware state to the temporary file. """
+        with open(STATE_FILE, 'w', encoding="utf-8") as file:
+            json.dump(self.state, file)
+        logger.debug("Soundcard state saved %s", self.state)
+
+    def load_state(self):
+        """ Load the hardware state from the temporary file. """
+        if os.path.exists(STATE_FILE):
+            with open(STATE_FILE, 'r', encoding="utf-8") as file:
+                try:
+                    self.state = json.load(file)
+                except json.JSONDecodeError:
+                    logger.warning("Failed to load soundcard state.")
+
+                logger.debug("Soundcard state loaded %s", self.state)
+                self.gain = self.state['gain']
+                self.phantom_mode = self.state['phantom']
+        else:
+            self.gain = 0
+            self.phantom_mode = 0
+            self.write_state()
+
     def enable(self):
-        """ Turn on the soundcard power rails"""
+        """ Turn on the soundcard power rails, set gain to 0, and disable phantom power """
         GPIO.output(EXT_MIC_EN, 1)
+        self.set_gain(0)
+        self.set_phantom(self.NONE)
+        self.write_state()
 
     def disable(self):
         """ Turn off the soundcard power rails"""
@@ -44,6 +94,8 @@ class Soundcard:
         tx[1] |= self.phantom_mode
 
         self.spi.xfer(tx)
+        self.state = {'gain':self.gain, 'phantom':self.phantom_mode}
+        self.store_state()
 
     def set_gain(self, gain):
         """ Set the gain of the soundcard """
