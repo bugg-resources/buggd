c4e04a992aa8f29bef9b377fdb79f4c0fa5131ae
Jeff Gough <j@jeffreygough.com>
2024-03-28 23:51:19 +0000

    For some reason reset_input_buffer doesn't work. Use brute force approach

--- src/bugg_recording/drivers/modem.py
+++ src/bugg_recording/drivers/modem.py
@@ -186,7 +186,7 @@ class Modem:
                 self.port = serial.Serial(CONTROL_INTERFACE, CONTROL_INTERFACE_BAUD, timeout=CONTROL_INTERFACE_TIMEOUT)
                 self.port.reset_input_buffer()
                 self.port.write("ATE0\r\n".encode())  # Turn off echo
-                self.port.reset_input_buffer()
+                self.port.read(CONTROL_INTERFACE_READ_SIZE)
             except serial.SerialException as e:
                 logger.error("Failed to open control interface: %s", e)
                 raise
