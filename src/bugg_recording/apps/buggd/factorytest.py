aa290d194278e63f2a4cdcf9c65feebebae865d8
Jeff Gough <j@jeffreygough.com>
2024-03-29 12:23:22 +0000

    Flesh out tests and test process

--- src/bugg_recording/apps/buggd/factorytest.py
+++ src/bugg_recording/apps/buggd/factorytest.py
@@ -13,12 +13,22 @@ class FactoryTest:
             "modem_responsive": False,
             "modem_sim_readable": False,
             "modem_towers_found": False,
+            "i2s_bridge_responding": False,
+            "rtc_responding": False,
+            "led_controller responding": False,
+            "internal_microphone_signal_present": False,
+            "external_microphone_signal_present": False,
         }
 
     def run(self):
         self.logger.info("Factory test running.")
         
+        self.logger.info("Testing modem.")
         self.test_modem() 
+        self.logger.info("Testing I2C devices.")
+        self.test_i2c_devices()
+        self.logger.info("Testing recording.")
+        self.test_recording()
         
         self.logger.info("Factory test completed.")
         return True
@@ -40,3 +50,9 @@ class FactoryTest:
         self.results["modem_responsive"] = self.modem_is_responsive()
         self.results["modem_sim_readable"] = self.modem_is_sim_readable()
         self.results["modem_towers_found"] = self.modem_towers_found()
+
+    def test_i2c_devices(self):
+        pass
+    
+    def test_recording(self):
+        pass
