65b91ac20edcc0ec345368a07f5c9648a6006e3f
Jeff Gough <j@jeffreygough.com>
2024-03-29 01:52:26 +0000

    More desperate delays

--- src/bugg_recording/drivers/modem.py
+++ src/bugg_recording/drivers/modem.py
@@ -185,7 +185,8 @@ class Modem:
         if self.port is None:
             try:
                 logger.debug("Opening port...")
-                self.port = serial.Serial(CONTROL_INTERFACE, CONTROL_INTERFACE_BAUD, timeout=CONTROL_INTERFACE_TIMEOUT)
+                self.port = serial.Serial(CONTROL_INTERFACE, CONTROL_INTERFACE_BAUD, exclusive=True, timeout=CONTROL_INTERFACE_TIMEOUT)
+                time.wait(2)
                 self.port.write("ATE0\r\n".encode())  # Turn off echo   
                 time.sleep(0.5) 
                 self.port.read_all()  # Clear the input buffer
