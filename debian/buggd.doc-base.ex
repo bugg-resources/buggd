c899e8b2d62ce5aa8182cf58bbacaacb85bde939
root <j@monad.is>
2024-03-31 19:39:16 +0000

    Add debian packaging files with initial config

---
+++ debian/buggd.doc-base.ex
@@ -0,0 +1,20 @@
+Document: buggd
+Title: Debian buggd Manual
+Author: <insert document author here>
+Abstract: This manual describes what buggd is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/buggd/buggd.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/buggd/buggd.ps.gz
+
+Format: text
+Files: /usr/share/doc/buggd/buggd.text.gz
+
+Format: HTML
+Index: /usr/share/doc/buggd/html/index.html
+Files: /usr/share/doc/buggd/html/*.html
