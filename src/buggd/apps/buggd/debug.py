77ae84fa0c1dcf26f8cf495fcca413160764e8b2
Jeff Gough <j@jeffreygough.com>
2024-04-04 06:31:10 +0100

    Change to using self.logger

--- src/buggd/apps/buggd/debug.py
+++ src/buggd/apps/buggd/debug.py
@@ -2,66 +2,65 @@ import traceback
 import sys
 
 import logging
-logger = logging.getLogger(__name__)
 
 # NOTE: to enable tracebacks, set the logger level to logging.DEBUG
 # and set ENABLE_TRACEBACKS to True
-logger.setLevel(logging.DEBUG)
 ENABLE_TRACEBACKS = False
 
 class DebugClass:
     """ Class that demonstrates logging at different levels"""
     def __init__(self):
-        pass
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
 
     def hello_logger(self):
         """ Method that demonstrates logging at different levels"""
-        logger.debug('logging.DEBUG from DebugClass')
-        logger.info('logging.INFO from DebugClass')
-        logger.warning('logging.WARNING from DebugClass')
-        logger.error('logging.ERROR from DebugClass')
-        logger.critical('logging.CRITICAL from DebugClass')
+        self.logger.debug('logging.DEBUG from DebugClass')
+        self.logger.info('logging.INFO from DebugClass')
+        self.logger.warning('logging.WARNING from DebugClass')
+        self.logger.error('logging.ERROR from DebugClass')
+        self.logger.critical('logging.CRITICAL from DebugClass')
 
 
-def write_traceback_to_log(exception):
-    """
-    Print detailed information about an exception, including the file, class, and line number where it occurred, 
-    along with a stack trace.
-    """
-    if not ENABLE_TRACEBACKS:
-        logger.debug('Tracebacks are disabled')
-        return
+    def write_traceback_to_log(self):
+        """
+        Print detailed information about an exception, including the file, class, and line number where it occurred, 
+        along with a stack trace.
+        """
+        if not ENABLE_TRACEBACKS:
+            self.logger.debug('Tracebacks are disabled')
+            return
 
-    # Fetching the current exception information
-    exc_type, exc_value, exc_traceback = sys.exc_info()
+        # Fetching the current exception information
+        exc_type, exc_value, exc_traceback = sys.exc_info()
 
-    # Extracting the stack trace
-    tb_lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-    logger.debug('Detailed Exception Traceback:')
-    logger.debug(''.join(tb_lines))
+        # Extracting the stack trace
+        tb_lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        self.logger.debug('Detailed Exception Traceback:')
+        self.logger.debug(''.join(tb_lines))
 
-    # Extract the last traceback object which points to where the exception was raised
-    while exc_traceback.tb_next:
-        exc_traceback = exc_traceback.tb_next
-    frame = exc_traceback.tb_frame
+        # Extract the last traceback object which points to where the exception was raised
+        while exc_traceback.tb_next:
+            exc_traceback = exc_traceback.tb_next
+        frame = exc_traceback.tb_frame
 
-    # Extracting file name (module) and line number
-    file_name = frame.f_code.co_filename
-    line_number = exc_traceback.tb_lineno
+        # Extracting file name (module) and line number
+        file_name = frame.f_code.co_filename
+        line_number = exc_traceback.tb_lineno
 
-    # Attempting to extract the class name, if any
-    class_name = frame.f_locals.get('self', None).__class__.__name__ if 'self' in frame.f_locals else None
+        # Attempting to extract the class name, if any
+        class_name = frame.f_locals.get('self', None).__class__.__name__ if 'self' in frame.f_locals else None
 
-    # logger.debuging extracted details
-    logger.debug(f'Exception occurred in file: {file_name}')
-    if class_name:
-        logger.debug(f'Exception occurred in class: {class_name}')
-    logger.debug(f'Exception occurred at line: {line_number}')
+        # self.logger.debuging extracted details
+        self.logger.debug('Exception occurred in file: %s', file_name)
+        if class_name:
+            self.logger.debug('Exception occurred in class: %s', class_name)
+        self.logger.debug('Exception occurred at line: %s, ', line_number)
 
 
-def divide_by_zero():
-    """ Function that raises a divide by zero exception """
-    try:
-        _ = 1/0
-    except Exception as e:
-        write_traceback_to_log(e)
+    def divide_by_zero(self):
+        """ Function that raises a divide by zero exception """
+        try:
+            _ = 1/0
+        except Exception:
+            self.write_traceback_to_log()
